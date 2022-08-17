3d4be206aa28116b52a8f2262fe21ec34cee2a3d
diegorodd <dieegoa@icloud.com>
2022-08-16 23:21:45 -0400

    added some readme

--- README.md
+++ README.md
@@ -1,1 +1,14 @@
 # Tech-Blog
+
+# Description 
+online users to publish articles, blog posts, and their thoughts and opinions.
+
+# Bulit with 
+•connect-session-sequelize
+•dotenv
+•express-handlebars
+•mysql2
+•sequelize
+•express
+•express-session
+• bcrypt
--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const express = require('express');
 const session = require('express-session');
 const exphbs = require('express-handlebars');
 const helpers = require('./utils/helpers');
-
+// make sure to make it a port
 const app = express();
 const PORT = process.env.PORT || 3001;
 
@@ -20,3 +20,21 @@ const sess = {
     })
 };
 
+app.use(session(sess));
+
+const hbs = exphbs.create({ helpers });
+
+app.engine('handlebars', hbs.engine);
+app.set('view engine', 'handlebars');
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use(require('./controllers/'));
+
+// Turn on connection to db and server
+
+sequelize.sync({ force: false }).then(() => {
+    app.listen(PORT, () => console.log('Now listening'));
+});
